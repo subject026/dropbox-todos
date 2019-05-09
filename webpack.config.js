06d0bf36c0741c256e6b6e8f029fef7a0d743468
subject026 <sudobuildspeakers@gmail.com>
2019-05-09 22:22:51 +0100

    optimized prod bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = envIsProd => {
   let appUrl;
+  let devtool;
   if (envIsProd) {
     appUrl = new webpack.DefinePlugin({
       APP_URL: JSON.stringify("https://cranky-goldberg-d11d75.netlify.com")
     });
+    devtool = false;
   } else {
     appUrl = new webpack.DefinePlugin({
       APP_URL: JSON.stringify("http://localhost:1111")
     });
+    devtool = "cheap-eval-source-map";
   }
   return {
     entry: ["babel-polyfill", "./src/js/app.js"],
@@ -19,7 +23,7 @@ module.exports = envIsProd => {
       path: path.join(__dirname, "dist"),
       filename: "js/bundle.js"
     },
-    devtool: "cheap-eval-source-map",
+    devtool,
     devServer: {
       port: "1111",
       contentBase: "./dist"
@@ -29,7 +33,8 @@ module.exports = envIsProd => {
         filename: "index.html",
         template: "./src/index.html"
       }),
-      appUrl
+      appUrl,
+      new BundleAnalyzerPlugin()
     ],
     module: {
       rules: [
@@ -46,10 +51,5 @@ module.exports = envIsProd => {
         }
       ]
     }
-    // resolve: {
-    //   alias: {
-    //     config: path.join(__dirname, "config", process.env.NODE_ENV)
-    //   }
-    // }
   };
 };
