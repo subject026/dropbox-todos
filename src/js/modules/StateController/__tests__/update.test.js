131dcdc723d3c83935a598e6e473a3dafe17929a
subject026 <sudobuildspeakers@gmail.com>
2019-06-23 23:18:04 +0100

    added more todos in tests

--- src/js/modules/StateController/__tests__/update.test.js
+++ src/js/modules/StateController/__tests__/update.test.js
@@ -1,5 +1,12 @@
 import { initialState, update } from "../index";
 
+const testTodos = [
+  { id: "1", title: "first todo item", isComplete: false },
+  { id: "2", title: "second todo item", isComplete: true },
+  { id: "3", title: "third todo item", isComplete: true },
+  { id: "4", title: "fourth todo item", isComplete: false }
+];
+
 describe("State Update Function", () => {
   it("'null' action should return state unchanged", () => {
     const result = update(null, {}, initialState);
@@ -7,21 +14,32 @@ describe("State Update Function", () => {
     expect(result).toEqual(initialState); // new obj should match state passed in
   });
 
-  it("ADD action should add new todo to state", () => {
-    const newTodo = { id: "1", title: "first todo item", isComplete: false };
-    const result = update("ADD", newTodo, initialState);
-    expect(result.todos).toHaveLength(1);
-    expect(result.todos[0]).toBe(newTodo);
+  it("ADD action should add new todo items to state", () => {
+    let newState = JSON.parse(JSON.stringify(initialState));
+    testTodos.forEach(todo => {
+      newState = update("ADD", todo, newState);
+    });
+    expect(newState.todos).toHaveLength(4);
+    expect(newState.todos[0]).toEqual(testTodos[0]);
+    expect(newState.todos[1]).toEqual(testTodos[1]);
+    expect(newState.todos[2]).toEqual(testTodos[2]);
+    expect(newState.todos[3]).toEqual(testTodos[3]);
   });
 
   it("TOGGLE action should toggle isComplete value", () => {
-    const newTodo = { id: "1", title: "first todo item", isComplete: false };
-    const stateWithTodo = update("ADD", newTodo, initialState);
-    // toggle to true
-    const toggledState1 = update("TOGGLE", { id: newTodo.id }, stateWithTodo);
-    expect(toggledState1.todos[0].isComplete).toBe(true);
-    // back to false
-    const toggledState2 = update("TOGGLE", { id: newTodo.id }, toggledState1);
-    expect(toggledState2.todos[0].isComplete).toBe(false);
+    let stateWithTodos = JSON.parse(JSON.stringify(initialState));
+    testTodos.forEach(todo => {
+      stateWithTodos = update("ADD", todo, stateWithTodos);
+    });
+    // false to true
+    let toggledState = update("TOGGLE", { id: testTodos[0].id }, stateWithTodos);
+    expect(toggledState.todos[0].isComplete).toBe(true);
+    // true to false
+    toggledState = update("TOGGLE", { id: testTodos[2].id }, toggledState);
+    expect(toggledState.todos[2].isComplete).toBe(false);
   });
+
+  xit("TODO_TITLE action should update a todo's title", () => {});
+
+  xit("LIST_TITLE action should update the list's title", () => {});
 });
