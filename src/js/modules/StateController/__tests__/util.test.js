2a7325644a630c7107dcbbde1b5a968bb0b41040
subject026 <sudobuildspeakers@gmail.com>
2019-06-24 15:41:32 +0100

    ++ test to check ids are unique

--- src/js/modules/StateController/__tests__/util.test.js
+++ src/js/modules/StateController/__tests__/util.test.js
@@ -1,4 +1,4 @@
-import { getTodoIndex } from "../Util";
+import { getTodoIndex, generateID } from "../Util";
 
 const testArr = [{ id: "A1" }, { id: "B2" }, { id: "C3" }, { id: "D4" }, { id: "E5" }];
 
@@ -13,3 +13,23 @@ describe("State Util Functions - getTodoIndex", () => {
     expect(result).toBe(false);
   });
 });
+
+// Tested 20,000 ids - all unique
+
+// describe("generateID should generate a unique ID", () => {
+//   xit("array of ids should have no duplicates", () => {
+//     const idArr = [];
+//     for (let i = 0; i < 20000; i++) {
+//       idArr.push(generateID());
+//     }
+//     const result = idArr.reduce(
+//       (acc, item) => {
+//         if (acc.checked.includes(item)) acc.duplicates.push(item);
+//         acc.checked.push(item);
+//         return acc;
+//       },
+//       { checked: [], duplicates: [] }
+//     );
+//     expect(result.duplicates).toHaveLength(0);
+//   });
+// });
