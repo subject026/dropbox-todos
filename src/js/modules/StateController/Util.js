7c9f3a798012e553213189ddd3f4c8312daa664c
subject026 <sudobuildspeakers@gmail.com>
2019-06-24 13:39:54 +0100

    ++ seperate findIndex fn

--- src/js/modules/StateController/Util.js
+++ src/js/modules/StateController/Util.js
@@ -5,11 +5,16 @@ function generateNumber() {
   return Math.floor(Math.random() * alphabet.length);
 }
 
-export default function generateID() {
+export function generateID() {
   let id = "";
   for (let i = 0; i < idLength; i++) {
     const num = generateNumber();
     id += alphabet[num];
   }
   return id;
+}
+
+export function getTodoIndex(arr, id) {
+  const index = arr.findIndex(item => item.id === id);
+  return index;
 }
