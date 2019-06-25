d0d3c424033c1011a130f424e8be4592f9f9188a
subject026 <sudobuildspeakers@gmail.com>
2019-06-25 11:48:48 +0100

    removed old console log

--- src/js/modules/DropboxController/DBX.js
+++ src/js/modules/DropboxController/DBX.js
@@ -7,7 +7,6 @@ let instance;
 export default {
   getInstance: function() {
     if (!instance) {
-      console.log("instantiating dbx...");
       instance = new Dropbox({ accessToken: getTokenLocal(), fetch });
     }
     return instance;
