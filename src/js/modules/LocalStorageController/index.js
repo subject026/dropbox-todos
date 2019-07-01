88725ce3447991ef0137d576f41e85d796b5324b
subject026 <sudobuildspeakers@gmail.com>
2019-07-01 10:22:56 +0100

    added new appData fns to localStorage mod

--- src/js/modules/LocalStorageController/index.js
+++ src/js/modules/LocalStorageController/index.js
@@ -5,3 +5,21 @@ export function getTokenLocal() {
 export function setTokenLocal(token) {
   localStorage.setItem("dropboxTodoToken", token);
 }
+
+export function getAppData() {
+  let data;
+  try {
+    data = JSON.parse(localStorage.getItem("appData"));
+  } catch (err) {
+    console.error(err);
+  }
+  return data;
+}
+
+export function setAppData(data) {
+  try {
+    localStorage.setItem("appData", JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+}
