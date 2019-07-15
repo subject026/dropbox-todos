65bf350b5c668c79e6c74dc2e56f5ef09365438f
subject026 <sudobuildspeakers@gmail.com>
2019-07-15 12:51:47 +0100

    added sw reg, manifest link.

--- src/js/modules/Init.js
+++ src/js/modules/Init.js
@@ -3,6 +3,19 @@ import { bindEvents } from "./Controller";
 import { getState } from "./Model";
 
 export default function init() {
+  // register SW
+  if ("serviceWorker" in navigator) {
+    // register our service worker
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   renderList(getState());
   bindEvents();
   // render build date time if env is prod
