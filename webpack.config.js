8fd2d7028616d7fd9c24f1c53abbcff7a1f188fa
subject026 <sudobuildspeakers@gmail.com>
2019-07-15 11:27:23 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const webpack = require("webpack");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const HTMLPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = envIsProd => {
   let appUrl, devtool;
@@ -30,12 +32,25 @@ module.exports = envIsProd => {
       contentBase: "./dist"
     },
     plugins: [
-      new HTMLWebpackPlugin({
+      new HTMLPlugin({
         filename: "index.html",
         template: "./src/index.html"
       }),
-      appUrl
-      // new BundleAnalyzerPlugin()
+      appUrl,
+      new WorkboxPlugin.InjectManifest({
+        swSrc: "./src/src-sw.js",
+        swDest: "sw.js"
+      }),
+      new CopyPlugin([
+        {
+          from: "./src/manifest.json",
+          to: "manifest.json"
+        },
+        {
+          from: "./src/images",
+          to: "images"
+        }
+      ])
     ],
     module: {
       rules: [
