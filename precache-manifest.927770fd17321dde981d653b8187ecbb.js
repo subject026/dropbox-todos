315e68116090a110ed14e10525aac37843bd179f
subject026 <sudobuildspeakers@gmail.com>
2019-07-18 12:22:05 +0100

    Updates

---
+++ precache-manifest.927770fd17321dde981d653b8187ecbb.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f978bd7e50bc7e5d2b00491c09dba97",
+    "url": "index.html"
+  },
+  {
+    "revision": "35be341add4780e8cc06",
+    "url": "js/bundle.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.d0654849b6f56bf5e24a52fc34424993.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.927770fd17321dde981d653b8187ecbb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
