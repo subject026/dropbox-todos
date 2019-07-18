8edc358e4e0a4000bed5404a39fd42b34aec328f
subject026 <sudobuildspeakers@gmail.com>
2019-07-18 12:14:32 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.15b35670b15d8c72e2ab350a9909c726.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d0654849b6f56bf5e24a52fc34424993.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
